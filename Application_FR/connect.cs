499cfab783a48b468129c9e85dc5313c06adada0
FodorRobert <fodorr@kkszki.hu>
2025-11-12 08:01:05 +0100

    Project is started

---
+++ Application_FR/connect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_FR
+{
+    internal class connect
+    {
+
+        public MySqlConnection _connection;
+
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string _connectionstring;
+
+        public connect()
+        {
+            _host = "localhost";
+            _db = "application";
+            _user = "root";
+            _password = "";
+
+            _connectionstring = $"SERVER=(_host);DATABASE=(_db);UID=(_user);PASSWORD=(_password);SslMode=None";
+
+            _connection = new MySqlConnection(_connectionstring);
+        }
+
+    }
+}
